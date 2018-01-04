a2d322fb2b4cbe574aa0af857f04f9ea48552832
Noble Millie <noble.millie@gmail.com>
2018-01-03 23:45:56 -0600

    static file

--- index.js
+++ index.js
@@ -1,106 +1,117 @@
+var canvas = document.getElementById('container');
+var clone = document.getElementById('blurCanvasBottom');
+
+var cloneCtx = clone.getContext('2d');
+var ctx = canvas.getContext('2d');
 
 
-<script>/*<![CDATA[*/var oldieCheck=Boolean(document.getElementsByTagName('html')[0].className.match(/\soldie\s/g));if(!oldieCheck){document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"><\/script>');}else{document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"><\/script>');}/*]]>*/</script>
-<script>/*<![CDATA[*/if(!window.jQuery){if(!oldieCheck){document.write('<script src="js/libs/jquery-2.0.2.min.js"><\/script>');}else{document.write('<script src="js/libs/jquery-1.10.1.min.js"><\/script>');}}/*]]>*/</script>
-<script src="js/libs/gumby.min.js"></script>
-<script src="js/plugins/waypoints.min.js"></script>
-<script src="js/plugins/placeholders.js"></script>
-<script src="js/theme.js"></script>
-<script>
-  $.get('https://api.github.com/repos/theodi/comma-chameleon/releases', function(data) {
-    assets = data[0].assets
-    $('#win32').attr('href', assets[3].browser_download_url)
-    $('#win64').attr('href', assets[4].browser_download_url)
-    $('#osx').attr('href', assets[0].browser_download_url)
-    $('#linux32').attr('href', assets[1].browser_download_url)
-    $('#linux64').attr('href', assets[2].browser_download_url)
-  })
-</script>
+var w = $('#blurCanvasTop').width();
+var h = $('#blurCanvasTop').height();
 
-<script>
-var odi_path = [
-  /* move to start */
-  'M 193 64',
+var ww = $(window).width();
+var wh = $(window).height();
+canvas.width = ww;
+canvas.height = wh;
+var partCount = 100;
+var particles = [];
 
-  /* 'O' */
-  'c -5 -12.4 -12.4 -23.8 -22.2 -33.6',
-  'c -18.9 -18.9 -43.9 -29.3 -70.6 -29.3',
-  'c -26.7 0 -51.8 10.4 -70.6 29.3',
-  'c -18.9 18.9 -29.2 43.9 -29.2 70.6',
-  'c 0 26.7 10.4 51.8 29.2 70.6',
-  'c 18.9 18.9 43.9 29.3 70.6 29.3',
-  'c 26.7 0 51.8 -10.4 70.6 -29.3',
-  'c 9.7 -9.7 17.2 -21.1 22.2 -33.6',
+function particle() {
+  this.color = 'rgba(255,255,255,' + Math.random() + ')';
+  console.log(this.color);
+  this.x = randomInt(0, ww);
+  this.y = randomInt(0, wh);
+  this.direction = {
+    "x": -1 + Math.random() * 2,
+    "y": -1 + Math.random() * 2
+  };
+  this.vx = 0.3 * Math.random();
+  this.vy = 0.3 * Math.random();
+  this.radius = randomInt(2, 3);
+  this.float = function() {
+    this.x += this.vx * this.direction.x;
+    this.y += this.vy * this.direction.y;
+  };
+  this.changeDirection = function(axis) {
+    this.direction[axis] *= -1;
+  };
+  this.boundaryCheck = function() {
+    if (this.x >= ww) {
+      this.x = ww;
+      this.changeDirection("x");
+    } else if (this.x <= 0) {
+      this.x = 0;
+      this.changeDirection("x");
+    }
+    if (this.y >= wh) {
+      this.y = wh;
+      this.changeDirection("y");
+    } else if (this.y <= 0) {
+      this.y = 0;
+      this.changeDirection("y");
+    }
+  };
+  this.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fill();
+  };
+}
 
-  /* bottom of 'D' */
-  'v 61.3',
-  'h 84.8',
-  'c 14.7 0 28.4 -2.4 40.9 -7',
-  'c 12.3 -4.6 23.1 -11.3 32 -19.9',
-  'c 10 -9.6 17.4 -21.2 22.2 -34.3',
+function clearCanvas() {
+  cloneCtx.clearRect(0, 0, ww, wh);
+  ctx.clearRect(0, 0, ww, wh);
+}
 
-  /* 'I' */
-  'v 61',
-  'h 93.5',
-  'v -197',
-  'h -93.5',
-  'v 61.1',
+function createParticles() {
+  for (i = 0; i < partCount; i++) {
+    var p = new particle();
+    particles.push(p);
+  }
+}
 
-  /* top of 'D' */
-  'c -4.8 -13.2 -12.2 -24.7 -22.2 -34.3',
-  'c -9 -8.6 -19.8 -15.3 -32 -19.9',
-  'c -12.5 -4.6 -26.2 -7 -40.9 -7',
-  'h -84.8',
-  'v 61.4'
-]
+function drawParticles() {
+  for (i = 0; i < particles.length; i++) {
+    p = particles[i];
+    p.draw();
+  }
+}
 
+function updateParticles() {
+  for (var i = particles.length - 1; i >= 0; i--) {
+    p = particles[i];
+    p.float();
+    p.boundaryCheck();
+  }
+}
+createParticles();
+drawParticles();
 
-var logo = SVG('odi-logo')
+function animateParticles() {
+  clearCanvas();
+  drawParticles();
+  updateParticles();
+  cloneCtx.drawImage(canvas, 0, 0);
+  requestAnimationFrame(animateParticles);
+}
+requestAnimationFrame(animateParticles);
+cloneCtx.drawImage(canvas, 0, 0);
 
-logo.width('45%')
-logo.height('100%')
-logo.id('odi-logo')
+$(window).on('resize', function() {
+  ww = $(window).width();
+  wh = $(window).height();
+  canvas.width = ww;
+  canvas.height = wh;
+  clearCanvas();
+  particles = [];
+  createParticles();
+  drawParticles();
+});
 
-logo.viewbox({
-  x: 0,
-  y: 0,
-  width: 467,
-  height: 202
-})
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-var badge = logo.path(odi_path.join(' ')).fill('#fff')
-
-logo.viewbox({
-  x: 0,
-  y: 0,
-  width: 830,
-  height: 200
-})
-
-var text = logo.text(function(add) {
-  add.tspan('open').newLine()
-  add.tspan('data').newLine()
-  add.tspan('institute').newLine()
-})
-
-
-//text.fill('#fa8100')
-
-text.move(482, -10)
-text.font({
-  family: 'Helvetica Neue',
-  size: 90,
-  anchor: 'left',
-  weight: 'bold',
-  leading: 0.8,
-  fill: '#fff'
-})
-
-var group = logo.group()
-group.add(badge)
-group.add(text)
-
-group.linkTo(function(link) {
-  link.to('//theodi.org')
-})
-</script>
+function velocityInt(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
